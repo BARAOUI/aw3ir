666a48e2e1c0578c8e5fe3319e26332e68013e2c
BARAOUI <92264104+BARAOUI@users.noreply.github.com>
2021-10-18 21:40:49 +0200

    Add files via upload

--- tp2/tp2.js
+++ tp2/tp2.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function validation()
 {
  document.getElementById("error").innerHTML= "";
@@ -48,3 +49,55 @@ const dateNaissance = new Date(document.getElementById("Date").value);
     }
 
 }
+=======
+function validation()
+{
+ document.getElementById("error").innerHTML= "";
+     if(document.getElementById("Nom").value.length < 5)
+{
+         document.getElementById("error").innerHTML = "La saisie de votre Nom est obligatoire" ;
+    return false
+}
+
+    if(document.getElementById("Prenom").value.length<5)
+{
+        document.getElementById("error").innerHTML = "La saisie de votre Prénom est obligatoire" ;
+    return false
+
+}
+
+if(document.getElementById("Date").value.length<5)
+{
+        document.getElementById("error").innerHTML = "La saisie de votre date de naissance est obligatoire" ;
+    return false
+ 
+}
+
+const dateNaissance = new Date(document.getElementById("Date").value);
+    if (dateNaissance.getTime() > Date.now()) {
+      document.getElementById("Error").innerHTML = "Insérez  SVP une date valide";
+      return false;
+    }
+
+    if(document.getElementById("Adresse").value.length < 5)
+    {
+         document.getElementById("Adresse");  
+        document.getElementById("error").innerHTML = "La saisie de votre adresse est obligatoire" ;
+        return false
+    }
+
+    if(document.getElementById("Mail").value.length < 5 || document.getElementById("Mail").value.indexOf("@") == -1)
+    {
+        document.getElementById("Mail");
+        document.getElementById("error").innerHTML = document.getElementById("error").innerHTML + " Votre adresse email doit contenir un @";
+   return false;
+    }
+
+
+    if(document.getElementById("error").innerHTML == ""){
+        document.getElementById("resultat").innerHTML= "Bienvenue </d>" + document.getElementById("Nom").value;
+        return true;
+    }
+
+}
+>>>>>>> 3ac4d616e7a67ca178031097f2877a7de3e57939
