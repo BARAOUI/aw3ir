e2394c4b319d8532f7ec3850eaa294a961552151
BARAOUI <92264104+BARAOUI@users.noreply.github.com>
2021-10-18 20:44:55 +0200

    h

--- tp2/tp2.js
+++ tp2/tp2.js
@@ -1,13 +1,13 @@
 function validation()
 {
  document.getElementById("error").innerHTML= "";
-     if(document.getElementById("name").value.length < 5)
+     if(document.getElementById("Nom").value.length < 5)
 {
          document.getElementById("error").innerHTML = "La saisie de votre nom est obligatoire" ;
     return false
 }
 
-    if(document.getElementById("prénom").value.length<5)
+    if(document.getElementById("Prenom").value.length<5)
 {
         document.getElementById("error").innerHTML = "La saisie de votre prénom est obligatoire" ;
     return false
@@ -27,23 +27,23 @@ const dateNaissance = new Date(document.getElementById("Date").value);
       return false;
     }
 
-    if(document.getElementById("adresse").value.length < 5)
+    if(document.getElementById("Adresse").value.length < 5)
     {
-         document.getElementById("adresse");  
+         document.getElementById("Adresse");  
         document.getElementById("error").innerHTML = "La saisie de votre adresse est obligatoire" ;
         return false
     }
 
-    if(document.getElementById("mail").value.length < 5 || document.getElementById("mail").value.indexOf("@") == -1)
+    if(document.getElementById("Mail").value.length < 5 || document.getElementById("Mail").value.indexOf("@") == -1)
     {
-        document.getElementById("mail");
+        document.getElementById("Mail");
         document.getElementById("error").innerHTML = document.getElementById("error").innerHTML + "Il faut bien saisir votre adresse mail contenant @";
    return false;
     }
 
 
     if(document.getElementById("error").innerHTML == ""){
-        document.getElementById("resultat").innerHTML= "Formulaire validé Bienvenue </d>" + document.getElementById("name").value;
+        document.getElementById("resultat").innerHTML= "Formulaire validé Bienvenue </d>" + document.getElementById("Nom").value;
         return true;
     }
 
