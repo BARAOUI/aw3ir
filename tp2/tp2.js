3ac4d616e7a67ca178031097f2877a7de3e57939
BARAOUI <92264104+BARAOUI@users.noreply.github.com>
2021-10-18 21:34:06 +0200

    Add files via upload

--- tp2/tp2.js
+++ tp2/tp2.js
@@ -3,13 +3,13 @@ function validation()
  document.getElementById("error").innerHTML= "";
      if(document.getElementById("Nom").value.length < 5)
 {
-         document.getElementById("error").innerHTML = "La saisie de votre nom est obligatoire" ;
+         document.getElementById("error").innerHTML = "La saisie de votre Nom est obligatoire" ;
     return false
 }
 
     if(document.getElementById("Prenom").value.length<5)
 {
-        document.getElementById("error").innerHTML = "La saisie de votre prénom est obligatoire" ;
+        document.getElementById("error").innerHTML = "La saisie de votre Prénom est obligatoire" ;
     return false
 
 }
@@ -23,7 +23,7 @@ if(document.getElementById("Date").value.length<5)
 
 const dateNaissance = new Date(document.getElementById("Date").value);
     if (dateNaissance.getTime() > Date.now()) {
-      document.getElementById("Error").innerHTML = "Veuillez insérer une date valide";
+      document.getElementById("Error").innerHTML = "Insérez  SVP une date valide";
       return false;
     }
 
@@ -37,13 +37,13 @@ const dateNaissance = new Date(document.getElementById("Date").value);
     if(document.getElementById("Mail").value.length < 5 || document.getElementById("Mail").value.indexOf("@") == -1)
     {
         document.getElementById("Mail");
-        document.getElementById("error").innerHTML = document.getElementById("error").innerHTML + "Il faut bien saisir votre adresse mail contenant @";
+        document.getElementById("error").innerHTML = document.getElementById("error").innerHTML + " Votre adresse email doit contenir un @";
    return false;
     }
 
 
     if(document.getElementById("error").innerHTML == ""){
-        document.getElementById("resultat").innerHTML= "Formulaire validé Bienvenue </d>" + document.getElementById("Nom").value;
+        document.getElementById("resultat").innerHTML= "Bienvenue </d>" + document.getElementById("Nom").value;
         return true;
     }
 
