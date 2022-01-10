8a26bc201399635c93ccb5d5e11657282f111c56
BARAOUI <92264104+BARAOUI@users.noreply.github.com>
2022-01-10 14:52:22 +0100

    Add files via upload

---
+++ tp4/store.js
@@ -0,0 +1,34 @@
+/*
+store.js
+Script pour gérer la liste de contact en JSON
+
+Pour ajouter un contact:  contactStore.add(_name, _firsname, _date, _adress, _mail);
+Pour récuper la liste:    contactStore.getList();
+*/
+var contactStore = (function () {
+    // variable privée
+    var contactList = [];
+  
+    // Expose these functions via an interface while hiding
+    // the implementation of the module within the function() block
+  
+    return {
+      add: function (_Nom, _Prenom, _date, _Adresse, _email) {
+        var contact = {
+          Nom: _Nom,
+          Prenom: _Prenom,
+          date: _date,
+          Adresse: _Adresse,
+          email: _email,
+        };
+        // ajout du contact à la liste
+        contactList.push(contact);
+  
+        return contactList;
+      },
+  
+      getList: function () {
+        return contactList;
+      },
+    };
+  })();
